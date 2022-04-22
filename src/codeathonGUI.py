0fc27f1804b1f316c1e544c2a557578817fcc270
IzzetWizzard <78123383+IzzetWizzard@users.noreply.github.com>
2022-04-22 15:22:52 -0400

    added page two functionality

--- src/codeathonGUI.py
+++ src/codeathonGUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk    
 window = tk.Tk()
 pageOne = tk.Frame()
+pageTwo = tk.Frame()
 greeting = tk.Label(text = "Please insert your medical text to translate", master = pageOne)
 greeting.pack()
 entry = tk.Entry(fg = "black", bg = "white", width = 50, master = pageOne)
@@ -9,6 +10,10 @@ def translateTerms():
     textToTranslate = entry.get()
     pageOne.destroy()
     #Do all proccessing you need to do here
+    finalText = "Here is your translated text: "+ textToTranslate
+    finalOutput = tk.Label(text = finalText, master = pageTwo)
+    finalOutput.pack()
+    pageTwo.pack()
 button = tk.Button(master = pageOne, fg = "white", bg = "black", height = 20, width = 50, text = "Translate", command = translateTerms)
 button.pack()
 pageOne.pack()
