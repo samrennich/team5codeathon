66e7351dab4e5c262ca9ec17b43cfeff9e662ff0
SamRennich <samuelrennich@gmail.com>
2022-04-26 10:47:08 -0400

    added spanish and french functionality

--- src/text_to_speech/text_to_speech.py
+++ src/text_to_speech/text_to_speech.py
@@ -1,17 +1,25 @@
 from constants import *
+from translate.translate import *
 
 import azure.cognitiveservices.speech as speech_sdk
 
-ENDPOINT = "\'http://www.w3.org/2001/10/synthesis\'"
+ENDPOINT = "http://www.w3.org/2001/10/synthesis"
+VOICES = {
+    'en': "en-US-JennyNeural",
+    'es': "es-CU-BelkysNeural",
+    'fr': "fr-FR-BrigitteNeural"
+}
 
 # Says given text
 def say(text):
     speech_config = speech_sdk.SpeechConfig(TEXT_TO_SPEECH_KEY, REGION)
     speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
 
+    lang = get_language(text)
+
     responseSsml = " \
-        <speak version='1.0' xmlns=" + ENDPOINT + " xml:lang='en-US'> \
-            <voice name='en-GB-LibbyNeural'> \
+        <speak version='1.0' xmlns='" + ENDPOINT + "' xml:lang='" + lang + "'> \
+            <voice name='" + VOICES[lang] + "'> \
                 {} \
                 <break strength='weak'/> \
             </voice> \
