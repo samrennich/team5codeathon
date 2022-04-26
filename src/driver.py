e159351ded2ffc45be8798fc3131ca93331d41d2
SamRennich <samuelrennich@gmail.com>
2022-04-25 21:20:25 -0400

    testing different systems

--- src/driver.py
+++ src/driver.py
@@ -1,3 +1,14 @@
+from email.mime import audio
 from api.medical_dictionary import *
+from translation.translate import *
+import vlc
 
-print(definition_of("calistasdnics"))
+# p = vlc.MediaPlayer(audio_of("ibuprofen"))
+# p.play()
+
+# (defintion, audio_endpoint) = definition_of("ibuproffen")
+
+# print(defintion)
+# print(audio_endpoint)
+
+print(translate("Hello", 'fr'))
